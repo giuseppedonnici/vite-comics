d4bdc3ef1378432a2e933c4f1d64fd06d2e4b25a
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-13 16:30:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <img src="" alt="">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables";
+
+
+
+
+</style>
