fd444aa785ed663dd6931a7e1825e524fef08455
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-13 17:32:28 +0200

    Add: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,58 @@ export default {
     name: "AppHeader",
     data() {
         return {
-            
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false
+                },
+            ]
         }
     }
 }
@@ -11,26 +62,63 @@ export default {
 
 <template>
     <header>
-        <img src="" alt="">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="">
+            <ul>
+                <li v-for="link in links" :class="link.current ? 'active' : ''">
+                    <a :href="link.url">{{ link.text }}</a>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
 <style scoped lang="scss">
-@use "../style/partials/variables";
+@use "../style/partials/variables" as *;
+
+header {
+    height: 100px;
+    background-color: white;
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 60%;
+        height: 100%;
+        margin: 0 auto;
+    }
+
+    img {
+        width: 70px;
+    }
+
+    ul {
+        display: flex;
+        gap: 1.2rem;
+        height: 100%;
+        font-size: .7rem;
+        font-weight: bold;
+        font-stretch: ultra-condensed;
+        list-style-type: none;
+
+        li {
+            &.active {
+                color: $primary;
+                border-bottom: 5px solid $primary;
+            }
+    
+            &:hover {
+                color: $primary;
+            }
+    
+            a {
+                line-height: 100px;
+                text-decoration: none;
+                color: inherit;
+            }
+        }
+    }
 
 
-
-
+}
 </style>
