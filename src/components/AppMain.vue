07451507a0cb5db55b9fa3de33fd3f9fc0620700
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-13 17:55:40 +0200

    Add: AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,9 +10,39 @@ export default {
 </script>
 
 <template>
+    <main>
+        <div class="content">
+            <div class="container">
+            --> Content goes here &lt;--
+            </div>
+        </div>
 
+        <div class="cta">
+
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
-@use "../style/partials/variables";
+@use "../style/partials/variables" as *;
+
+main {
+    .content {
+        line-height: 100px;
+        color: white;
+        font-size: 2rem;
+        font-weight: bold;
+        font-stretch: ultra-condensed;
+        background-color: black;
+        .container {
+            width: 60%;
+            margin: 0 auto;
+        }
+    }
+    
+    .cta {
+        background-color: $primary;
+        height: 200px;
+    }
+}
 </style>
